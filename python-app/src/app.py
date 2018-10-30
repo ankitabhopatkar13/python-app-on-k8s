19d7dd8fcd017e1fe89eba880728161fc2e52ac1
Ankita Bhopatkar <bankita@thoughtworks.com>
2018-10-31 00:11:24 +0530

    Fixed dockerfile

--- python-app/src/app.py
+++ python-app/src/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, redirect
+
 app = Flask(__name__)
+
+APP_ENV = os.environ.get('APP_ENV')
+DEBUG = APP_ENV == 'development'
 
 
 def get_app_version():
     with open('VERSION.txt') as f:
         return f.read()
+
+
+@app.route('/')
+def home():
+    return redirect('/version', code=302)
 
 
 @app.route('/version')
@@ -13,4 +23,4 @@ def version():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=DEBUG)
